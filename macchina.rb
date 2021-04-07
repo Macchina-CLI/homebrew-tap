2e26e59f1278a8e8436666d48014605bdacce7b1
Marvin Haschker <123marvin123@users.noreply.github.com>
2021-04-07 20:25:43 +0200

    Update to v0.6.6

--- macchina.rb
+++ macchina.rb
@@ -1,8 +1,8 @@
 class Macchina < Formula
   desc "Basic system information fetcher, with a focus on performance and minimalism"
   homepage "https://github.com/Macchina-CLI/macchina"
-  url "https://github.com/Macchina-CLI/macchina/archive/refs/tags/0.6.5.tar.gz"
-  sha256 "cac97a5c6f61c5b8f5cb82cb19c8cc5db094d8465ad4a4fdddb5678c8e9827e2"
+  url "https://github.com/Macchina-CLI/macchina/archive/v0.6.6.tar.gz"
+  sha256 "a53c09e152c7f30c6a7959423d14e84d76f2d4c2efec5077930b308e230e36dd"
   license "MIT"
 
   pour_bottle? do

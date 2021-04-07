8aea91f2c8335f362185e741773064b9b914bdc8
Marvin Haschker <123marvin123@users.noreply.github.com>
2021-04-07 18:00:49 +0200

    Update to 0.6.5

--- macchina.rb
+++ macchina.rb
@@ -1,7 +1,7 @@
 class Macchina < Formula
   desc "Basic system information fetcher, with a focus on performance and minimalism"
   homepage "https://github.com/grtcdr/macchina"
-  url "https://github.com/grtcdr/macchina/archive/v0.6.2.tar.gz"
+  url "https://github.com/grtcdr/macchina/archive/v0.6.5.tar.gz"
   sha256 "a3ca59bf1099fbcf8d346bd2b490c9b4c69434f241bf2b3248f54c827a3bfe44"
   license "MIT"
 

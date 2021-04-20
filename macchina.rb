33e4786321e1bffdf6706d4b45afa3b895fa4c0d
Marvin Haschker <123marvin123@users.noreply.github.com>
2021-04-20 19:11:09 +0200

    Update to v0.7.0 Cobalt

--- macchina.rb
+++ macchina.rb
@@ -1,14 +1,9 @@
 class Macchina < Formula
   desc "Basic system information fetcher, with a focus on performance and minimalism"
   homepage "https://github.com/Macchina-CLI/macchina"
-  url "https://github.com/Macchina-CLI/macchina/archive/v0.6.8.tar.gz"
-  sha256 "135a7b4b4f6acd2a8595fa54a118f465a9e3c12c1f0fecfa931448eaac84424e"
+  url "https://github.com/Macchina-CLI/macchina/archive/v0.7.0.tar.gz"
+  sha256 "6ff1497864a400f5eade2b46984cbe1259a0f7a75bf7191a2a75e111a8ffe119"
   license "MIT"
-
-  pour_bottle? do
-    reason "The bottle needs the Xcode CLT to be installed."
-    satisfy { MacOS::CLT.installed? }
-  end
 
   depends_on "rust" => :build
 
